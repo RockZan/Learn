6e2694b1ee0c64dea1fbed73de6d022c6448c9b3
Rock <22829929+RockZan@users.noreply.github.com>
2018-06-14 22:18:29 +0800

    thermostat led indicator version

--- arduinoLrn/testleds/testfade/testfade.ino
+++ arduinoLrn/testleds/testfade/testfade.ino
@@ -13,20 +13,26 @@
   http://www.arduino.cc/en/Tutorial/Fade
 */
 
-int led = 10;           // the PWM pin the LED is attached to
+int ledG = 8;
+int ledY = 9;
+int ledR = 10;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledY, OUTPUT);
+  pinMode(ledR, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  analogWrite(ledG, brightness);
+  analogWrite(ledY, brightness);
+  analogWrite(ledR, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
